2ce9e432edf714e17c82501c77cb4acfa2ea20f8
brennogbi <brenoguanambi@hotmail.com>
2025-09-08 20:35:05 -0300

    atualizando contadores

--- Contas do Mês/script.js
+++ Contas do Mês/script.js
@@ -1,28 +1,28 @@
-const form = document.querySelector('form')
+const form = document.querySelector("form")
 const resposta1 = document.querySelector("#resposta1")
 const resposta2 = document.querySelector("#resposta2")
 
 
-let resposta = ""
-let valortotal = 0
-let nuncontas = 0
+let resposta = "" // string com a resposta a ser exibida
+let valortotal = 0 //aculula as variaveis
+let nuncontas = 0 // declara e inicializa o contador
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
 
 
-    const contas= form.contas.value
+    const contas= form.contas.value // obitem dados da conta
     const valor = Number(form.valor.value)
 
-    nuncontas ++
+    nuncontas++ // adiciona valores ao contador e acumulador
     valortotal = valortotal + valor
 
     resposta = resposta + contas+ " -R$:" + valor.toFixed(2)+ '\n'
 
-    resposta1.innerHTML= `${resposta}-------------------------`
-    resposta2.innerHTML = `${nuncontas} Contas(s) - Total R$: ${valor.toFixed(2)}`
+    resposta1.innerHTML= `${resposta}`
+    resposta2.innerHTML = `${nuncontas} Contas(s) - Total R$: ${valortotal.toFixed(2)}`
 
-    form.contas.value = ""
+    form.contas.value = "" // limpa od campos de form
     form.valor.value = ""
-    form.contas.focus()
+    form.contas.focus() //posiciona no campo contas
 })
